9fd6c317284c3893c5581cecc5dd17d8ba0c9502
zfx0726 <zfx0726@gmail.com>
2015-03-07 01:51:15 -0500

    new blog post

--- app/static/js/blog/blog-controller.js
+++ app/static/js/blog/blog-controller.js
@@ -1,7 +1,7 @@
 angular.module('splashapp')
   .controller('BlogController', ['$scope', function ($scope) {
 
-	$scope.postNum=1;
+	$scope.postNum=2;
 	
 	$scope.set = function(num){
 		$scope.postNum = num;
