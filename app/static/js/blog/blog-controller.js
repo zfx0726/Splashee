e339a4083cf9e0a3e32cc58a1f262791d74ac231
zfx0726 <zfx0726@gmail.com>
2015-03-15 00:06:07 -0400

    new blog, new recommendation system

--- app/static/js/blog/blog-controller.js
+++ app/static/js/blog/blog-controller.js
@@ -1,7 +1,7 @@
 angular.module('splashapp')
   .controller('BlogController', ['$scope', function ($scope) {
 
-	$scope.postNum=3;
+	$scope.postNum=4;
 	
 	$scope.set = function(num){
 		$scope.postNum = num;
--- app/static/js/home/home-controller.js
+++ app/static/js/home/home-controller.js
@@ -8,39 +8,74 @@ angular.module('splashapp')
   	
   	//json model here: http://www.w3schools.com/js/js_json.asp
   	
+  	
+  	$scope.clickedLabels= [];
+  	
+  	$scope.filters= ["Weather","Environment"];
+  	
   	$scope.inputPictures = [
-     {"category":"Weather", "paths":["../img/picPool/Ski.jpg", "../img/picPool/SunnyBeach.jpg","../img/picPool/Autumn.jpg"]},
-     {"category":"Environment", "paths":["../img/picPool/MountainLake.jpg", "../img/picPool/Village.jpg","../img/picPool/City.jpg"]},
-     {"category":"Company", "paths":["../img/picPool/CityGirl.jpg", "../img/picPool/Kiss.jpg","../img/picPool/Party.jpg"]},
-     {"category":"Activeness", "paths":["../img/picPool/BarcelonaChilling.jpg", "../img/picPool/ParkPic.jpg","../img/picPool/SurfsUp.jpg"]},
+     {"category":"Weather", "paths":["../img/picPool/Ski.jpg", "../img/picPool/SummerSunny.jpg","../img/picPool/Autumn.jpg"],"labels":
+     ["cold","warm","mild"]},
+     {"category":"Environment", "paths":["../img/picPool/MountainLake.jpg", "../img/picPool/Village.jpg","../img/picPool/City.jpg"],
+     "labels":["wild","village","urban"]},
+     //{"category":"Company", "paths":["../img/picPool/CityGirl.jpg", "../img/picPool/Kiss.jpg","../img/picPool/Party.jpg"],
+     //"labels":["solo","couple","group"]},
+     {"category":"Activeness", "paths":["../img/picPool/BarcelonaChilling.jpg", "../img/picPool/ParkPic.jpg","../img/picPool/SurfsUp.jpg"],
+     "labels":["relaxed","moderate","active"]},
      
  	]
  	
  	
  	$scope.experiencePictures = [
-     {"experience":"Songkran", "location":"in Chiang Mai, Thailand", "path":"../img/picPool/SongkranWatergun.jpg", "flightPrice":1338, "dailyPrice":20.02, "date": "April"},
+     {"experience":"Songkran", "location":"Chiang Mai, Thailand", "path":"../img/picPool/SongkranWatergun.jpg", 
+     "flightPrice":1338, "dailyPrice":20.02, "date": "April", "Weather":"warm", "Environment": "urban"},
      
-     {"experience":"Safari", "location":"in Kruger National Park, South Africa", "path":"../img/picPool/KrugerSafari.jpg", "flightPrice":1194, "dailyPrice":40, "date": "September"},
-     {"experience":"Ski", "location":"in the Swiss Alps", "path":"../img/picPool/SwissAlps.jpg", "flightPrice":696, "dailyPrice":175, "date": "November"},
-     {"experience":"Running of the Bulls", "location":"in Pamplona, Spain", "path":"../img/picPool/RunningBulls.jpg", "flightPrice":935, "dailyPrice":45, "date": "July"},
+     {"experience":"Safari", "location":"Kruger National Park, South Africa", "path":"../img/picPool/KrugerSafari.jpg", 
+     "flightPrice":1194, "dailyPrice":40, "date": "September", "Weather":"warm", "Environment":"village"},
      
-     {"experience":"Meditation Retreat", "location":"in Bali, Indonesia", "path":"../img/picPool/BaliRetreat.jpg", "flightPrice":1283, "dailyPrice":25, "date": "April"},
+     {"experience":"Ski", "location":"Swiss Alps", "path":"../img/picPool/SwissAlps.jpg", 
+     "flightPrice":696, "dailyPrice":175, "date": "November","Weather":"cold",
+     "Activeness":"active"},
      
-     {"experience":"Kitesurf", "location":"on Kite Beach, Dominican Republic", "path":"../img/picPool/Kitesurfing.jpg", "flightPrice":621, "dailyPrice":130, "date": "April"},
-     {"experience":"Camp", "location":"in Denali National Park, Alaska", "path":"../img/picPool/DenaliCamping.jpg", "flightPrice":526, "dailyPrice":25, "date": "July"},
-     {"experience":"Wine Tour", "location":"around Napa Valley, California", "path":"../img/picPool/NapaVineyard.jpg", "flightPrice":328, "dailyPrice":100, "date": "September"},
+     {"experience":"Running of the Bulls", "location":"Pamplona, Spain", "path":"../img/picPool/RunningBulls.jpg", 
+     "flightPrice":935, "dailyPrice":45, "date": "July", "Weather":"warm", 
+     "Activeness":"active"},
+     
+     {"experience":"Meditation Retreat", "location":"Bali, Indonesia", "path":"../img/picPool/BaliRetreat.jpg", 
+     "flightPrice":1283, "dailyPrice":25, "date": "April", "Weather":"warm", "Environment":"village",
+     "Activeness":"relaxed"},
+     
+     {"experience":"Kitesurf", "location":"Kite Beach, Dominican Republic", "path":"../img/picPool/Kitesurfing.jpg", 
+     "flightPrice":621, "dailyPrice":130, "date": "April", "Weather":"warm", "Environment":"village",
+     "Activeness":"active"},
+     
+     {"experience":"Camp", "location":"Denali National Park, Alaska", "path":"../img/picPool/DenaliCamping.jpg", 
+     "flightPrice":526, "dailyPrice":25, "date": "July", "Environment":"wild",
+     "Activeness":"active"},
+     
+     
+     {"experience":"Wine Tour", "location":"Napa Valley, California", "path":"../img/picPool/NapaVineyard.jpg", 
+     "flightPrice":328, "dailyPrice":100, "date": "September", "Weather":"mild"},
      
  	 
  	 
- 	 {"experience":"Scuba Dive", "location":"the Great Barrier Reef, Australia", "path":"../img/picPool/ScubaGreatReef.jpg", "flightPrice":1138, "dailyPrice":100, "date": "November"},
- 	 {"experience":"Pilgrimage", "location":"to Mecca", "path":"../img/picPool/MeccaPilgrimage.jpg", "flightPrice":926, "dailyPrice":25, "date": "May"},
+ 	 {"experience":"Scuba Dive", "location":"Great Barrier Reef, Australia", "path":"../img/picPool/ScubaGreatReef.jpg", 
+ 	 "flightPrice":1138, "dailyPrice":100, "date": "November", "Weather":"warm",
+     "Activeness":"active"},
+ 	 
+ 	 {"experience":"Pilgrimage to Mecca", "location":"Mecca, Saudi Arabia", "path":"../img/picPool/MeccaPilgrimage.jpg", 
+ 	 "flightPrice":926, "dailyPrice":25, "date": "May", "Weather":"warm", 
+     "Activeness":"active"},
  	 
  	 //{"experience":"Yacht Week", "location":"in Mecca", "path":"../img/picPool/MeccaPilgrimage.jpg", "flightPrice":926, "dailyPrice":25, "date": "May"},
  	 
  	 //{"experience":"Hiking", "location":"in Mecca", "path":"../img/picPool/MeccaPilgrimage.jpg", "flightPrice":926, "dailyPrice":25, "date": "May"},
  	 //{"experience":"Work on a Vineyard", "location":"in Mecca", "path":"../img/picPool/MeccaPilgrimage.jpg", "flightPrice":926, "dailyPrice":25, "date": "May"},
  	 //{"experience":"Yoga", "location":"in Mecca", "path":"../img/picPool/MeccaPilgrimage.jpg", "flightPrice":926, "dailyPrice":25, "date": "May"},
- 	 {"experience":"Oktoberfest", "location":"in Munich, Germany", "path":"../img/picPool/Oktoberfest.jpg", "flightPrice":782, "dailyPrice":65.82, "date": "September"},
+ 	   
+ 	 {"experience":"Oktoberfest", "location":"Munich, Germany", "path":"../img/picPool/Oktoberfest.jpg", 
+ 	 "flightPrice":782, "dailyPrice":65.82, "date": "September", "Weather":"mild" },
+ 	 
  	 //{"experience":"St Patrick's Day", "location":"in Mecca", "path":"../img/picPool/MeccaPilgrimage.jpg", "flightPrice":926, "dailyPrice":25, "date": "May"},
  	 //{"experience":"Adventure Sport", "location":"in New Zealand", "path":"../img/picPool/MeccaPilgrimage.jpg", "flightPrice":926, "dailyPrice":25, "date": "May"},
  	 //{"experience":"Surf", "location":"in Mecca", "path":"../img/picPool/MeccaPilgrimage.jpg", "flightPrice":926, "dailyPrice":25, "date": "May"},
@@ -66,18 +101,19 @@ angular.module('splashapp')
   	
   	$scope.forward = function(){
     	$scope.inputBatch= $scope.inputBatch + 1;
-    	getInputs();
+    	$scope.clickedLabels.push(null);
     }
     
     $scope.backward = function(){
     	$scope.inputBatch= $scope.inputBatch - 1;
-    	getInputs();
     }
 
 
 
 	$scope.nextStep = function(){
   			$scope.page= $scope.page + 1;
+  			
+  			//can do this only when page gets large enough
 			getRecs();
   	}
   	
@@ -99,8 +135,23 @@ angular.module('splashapp')
   				
   				if($scope.experiencePictures[index].flightPrice + $scope.experiencePictures[index].dailyPrice*$scope.tripLength<$scope.budget)
   				{
-  					tempArray[tempIndex]=$scope.experiencePictures[index];
-  					tempIndex= tempIndex+1;
+  					if($scope.experiencePictures[index].Weather==$scope.clickedLabels[0] || !$scope.experiencePictures[index].Weather 
+  					|| !$scope.clickedLabels[0]){
+  						
+  						if($scope.experiencePictures[index].Environment==$scope.clickedLabels[1] || 
+  						!$scope.experiencePictures[index].Environment || !$scope.clickedLabels[1]){
+  							
+  							if($scope.experiencePictures[index].Activeness==$scope.clickedLabels[2] || 
+  							!$scope.experiencePictures[index].Activeness || !$scope.clickedLabels[2]){
+  									tempArray[tempIndex]=$scope.experiencePictures[index];
+  									tempIndex= tempIndex+1;
+  								}
+  								
+  							}
+  							
+  						}
+  						
+  					
   				}
   				
   			}
@@ -223,10 +274,12 @@ angular.module('splashapp')
     $scope.wrappers=createChoices();
 
 
-  	$scope.inputClick = function(n){
-		$scope.inputBatch = n;
+  	$scope.inputClick = function(label){
+		$scope.inputBatch = $scope.inputBatch+1;
         $scope.backArrow = true;
-        	
+        
+        $scope.clickedLabels.push(label);
+        
         	
         if($scope.inputPictures.length-$scope.inputBatch == 0){
         	$scope.nextStep();
